437643b3649c676823c43f87a4cb5a64c26cc357
Mitesh Goswami <codemastermitesh@gmail.com>
2022-11-15 22:29:23 +0530

    modifying ceiling

--- src/org/mitesh/ds/binarysearchpattern/CeilingFloor.java
+++ src/org/mitesh/ds/binarysearchpattern/CeilingFloor.java
@@ -10,6 +10,9 @@ public class CeilingFloor {
     static int ceiling(int[] arr, int target){
         int start = 0;
         int end = arr.length -1;
+        if(target> arr[arr.length -1]){
+            return -1;
+        }
         while(start <= end){
             int mid = start + (end - start)/2;
             if(target < arr[mid]){
