58381556eaf79ea471c360bdf9bfcff6b4897006
Mitesh Goswami <codemastermitesh@gmail.com>
2022-11-22 23:20:55 +0530

    cyclic sort

---
+++ src/org/mitesh/ds/sort/CyclicSort.java
@@ -0,0 +1,29 @@
+package org.mitesh.ds.sort;
+
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = new int[]{4,1,2,3,5};
+        cyclicSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void cyclicSort(int[] arr){
+        int i = 0;
+        while(i<arr.length){
+            int correct = arr[i] - 1;
+            if(i != correct){
+                swap(arr,i,correct);
+            }
+            else{
+                i++;
+            }
+        }
+    }
+
+     static void swap(int[] arr, int i, int correct) {
+        int temp = arr[i];
+        arr[i] = arr[correct];
+        arr[correct] = temp;
+    }
+}
